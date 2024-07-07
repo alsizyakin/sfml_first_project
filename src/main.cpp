ba57641e108224f7a6a4a2113c7a58f22c2ab5a6
alsizyakin <al.sizyakin@gmail.com>
2024-07-07 15:30:21 +0300

    basically movement with tail realised

--- src/main.cpp
+++ src/main.cpp
@@ -1,198 +1,124 @@
 #include <SFML/Graphics.hpp>
 #include <unistd.h>
- 
-using namespace sf;
- 
-int main()
-{
-	// Объект, который, собственно, является главным окном приложения
-	RenderWindow window(VideoMode(960, 540), "Speedometer");
-    ContextSettings settings;
-	settings.antialiasingLevel = 8;
-    int coord_x = 0;
-	int coord_prev = 0;
-	int coord_prev2 = 0;
-	int coord_prev3 = 0;
+#include <vector>
 
-	int coord_prev4 = 0;
-	int coord_prev5 = 0;
-	int coord_prev6 = 0;
+// using namespace sf;
 
-    int dx = 1;
-	// Главный цикл приложения: выполняется, пока открыто окно
-	while (window.isOpen())
-	{
-	    // Обрабатываем события в цикле
-		Event event;
-		while (window.pollEvent(event))
-		{
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
-			if (event.type == Event::Closed)
-				// тогда закрываем его
-				window.close();
-		}
-		
-		coord_prev6 = coord_prev5;		
-		coord_prev5 = coord_prev4;
-		coord_prev4 = coord_prev3;		
-		coord_prev3 = coord_prev2;
-		coord_prev2 = coord_prev;
-		coord_prev = coord_x;
-		coord_x +=dx;  
+int main() {
 
-        if(((coord_x) > 120) or (coord_x < -120)){
-        dx *= -1;
-		
-        //coord_x = 0;
+    float arrow_angle = 0;
+    float cursor_angle = 0;
+    float d_angle = 0.5f;
+    float max_angle = 120;
+    float min_angle = -120;
+    int d_cursor = 1;
+    int tail_length = 25;
+    std::vector<float> tail;
+    int tail_pointer = 0;
+    int stop_ind = 4;
+    // SFML vars
+    //  Объект, который, собственно, является главным окном
+    //  приложения
+    sf::RenderWindow window(sf::VideoMode(960, 540), "Speedometer");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::Texture scale;
+    // Подгружаем текстуру шкалы из файла
+    scale.loadFromFile("oldcar.jpg");
+    scale.setSmooth(true);
+    sf::Texture arrow;
+    // Подгружаем нашу текстуру стрелки из файла
+    arrow.loadFromFile("arrow.png");
+    arrow.setSmooth(true);
+    // Главный цикл приложения: выполняется, пока открыто окно
+
+    tail.resize(tail_length);
+    while (window.isOpen()) {
+        // Обрабатываем события в цикле
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Пользователь нажал на «крестик» и хочет закрыть окно
+            if (event.type == sf::Event::Closed) {
+                // тогда закрываем его
+                window.close();
+            }
+        }
+
+        //  Создаем спрайт фона и устанавливаем ему текстуру шкалы
+        sf::Sprite sprite_scale(scale, sf::IntRect(0, 0, 754, 753));
+        sprite_scale.setOrigin(377, 377);
+
+        // sprite.setColor(Color(255,0,0,180));
+        //sprite_scale.move(377, 377);
+        // sprite.rotate(45);
+        sprite_scale.setScale(0.5f, 0.5f);
+        // Отрисовка спрайта
+        window.draw(sprite_scale);
+
+        // Заготовка фигуры многоугольника стрелки круиза
+        sf::ConvexShape convex;
+        // Устанавливаем ему 5 вершин
+        convex.setPointCount(4);
+        // Устанавливаем координаты вершин
+        convex.setPoint(0, sf::Vector2f(7.f, 0.f));
+        convex.setPoint(1, sf::Vector2f(3.f, 0.f));
+        convex.setPoint(2, sf::Vector2f(0.f, 60.f));
+        convex.setPoint(3, sf::Vector2f(12.f, 60.f));
+        convex.setOrigin(6, 120);
+        // Устанавливаем цвет многоугольника
+        convex.setFillColor(sf::Color(0, 169, 200, 255));
+        convex.setOutlineThickness(4.f);
+        convex.setOutlineColor(sf::Color(10, 180, 230, 155));
+        // Теперь сдвинем его вниз и чуть-чуть вправо
+        convex.move(377, 377);
+        convex.rotate(cursor_angle);
+        // Отрисовка многоугольника
+        window.draw(convex);
+
+
+        if (tail_pointer++ == tail.size()) {
+            tail_pointer = 0;
+            tail[tail_pointer] = tail[tail_length - 1];
+        } else {
+            tail[tail_pointer] = tail[tail_pointer - 1];
+        }
+        tail[tail_pointer] += d_angle;
+        if (tail[tail_pointer] > max_angle or tail[tail_pointer] < min_angle) {
+            d_angle *= -1.f;
+            if (--stop_ind == 0) {
+                d_angle = 0.f;
+            }
+        }
+
+        // Создаем спрайт и устанавливаем ему нашу
+        // текстуру texture.setRepeated(true);
+        int i = tail_pointer + 1;
+        int j = 0;
+        while (j < tail.size()) {
+            if (i > tail.size() - 1) {
+                i = 0;
+            }
+
+            sf::Sprite sprite_arrow(arrow, sf::IntRect(0, 0, 376, 79));
+            sprite_arrow.setOrigin(70, 40);
+
+            sprite_arrow.setColor(
+                sf::Color(250, 50 * !static_cast<bool>(j == tail_length - 1),
+                          100 * !static_cast<bool>(j == tail_length - 1),
+                          250 / tail_length * j));
+
+            sprite_arrow.move(377, 377);
+            sprite_arrow.rotate(tail[i] - 100);
+            sprite_arrow.setScale(0.5f, 0.5f);
+            // Отрисовка спрайта
+            window.draw(sprite_arrow);
+            j++;
+            i++;
+        }
+        // Отрисовка окна
+        window.display();
+        usleep(16667);
     }
-		// Установка цвета фона - белый
-		window.clear(Color::Black);
- 
-		// Создаем переменную текстуры
-		Texture scale;
- 
-	    // Подгружаем нашу текстуру из файла texture.png
-	    scale.loadFromFile("oldcar.jpg");
-        scale.setSmooth(true);
-		// Создаем спрайт и устанавливаем ему нашу текстуру
-        //texture.setRepeated(true);
-		Sprite sprite(scale, IntRect(0,0,796,768));
-        sprite.setOrigin(398,384);
 
-		//sprite.setColor(Color(255,0,0,180));
-
-        sprite.move(450,250);
-        //sprite.rotate(45);
-        sprite.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite);
-
-
-		// Создаем переменную текстуры
-		Texture arrow;
- 
-	    // Подгружаем нашу текстуру из файла texture.png
-	    arrow.loadFromFile("/home/alexey/Git/Comp/SFML/cmake-sfml-project/arrow.png");
-        arrow.setSmooth(true);
-		// Создаем спрайт и устанавливаем ему нашу текстуру
-        //texture.setRepeated(true);
-/*
-		Sprite sprite7(arrow, IntRect(0,0,376,79));
-        sprite7.setOrigin(70,40);
-
-		sprite7.setColor(Color(250,50,50,50));
-
-        sprite7.move(450,250);
-        sprite7.rotate(coord_prev6 - 100);
-        sprite7.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite7);
-
-
-		Sprite sprite6(arrow, IntRect(0,0,376,79));
-        sprite6.setOrigin(70,40);
-
-		sprite6.setColor(Color(250,50,50,50));
-
-        sprite6.move(450,250);
-        sprite6.rotate(coord_prev5 - 100);
-        sprite6.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite6);
-
-		Sprite sprite5(arrow, IntRect(0,0,376,79));
-        sprite5.setOrigin(70,40);
-
-		sprite5.setColor(Color(250,50,50,50));
-
-        sprite5.move(450,250);
-        sprite5.rotate(coord_prev4 - 100);
-        sprite5.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite5);
-
-		Sprite sprite4(arrow, IntRect(0,0,376,79));
-        sprite4.setOrigin(70,40);
-
-		sprite4.setColor(Color(250,50,50,50));
-
-        sprite4.move(450,250);
-        sprite4.rotate(coord_prev3 - 100);
-        sprite4.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite4);
-
-
-
-		Sprite sprite3(arrow, IntRect(0,0,376,79));
-        sprite3.setOrigin(70,40);
-
-		sprite3.setColor(Color(250,50,50,100));
-
-        sprite3.move(450,250);
-        sprite3.rotate(coord_prev2 - 100);
-        sprite3.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite3);
-
-
-
-		Sprite sprite2(arrow, IntRect(0,0,376,79));
-        sprite2.setOrigin(70,40);
-
-		sprite2.setColor(Color(250,50,50,150));
-
-        sprite2.move(450,250);
-        sprite2.rotate(coord_prev - 100);
-        sprite2.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite2);
-
-*/
-
-		Sprite sprite1(arrow, IntRect(0,0,376,79));
-        sprite1.setOrigin(70,40);
-
-		sprite1.setColor(Color(250,50,50,250));
-
-        sprite1.move(450,250);
-        sprite1.rotate(coord_x - 100);
-        sprite1.setScale(0.5f,0.5f);
-		// Отрисовка спрайта	 
-		window.draw(sprite1);
-
-
-        // Заготовка фигуры многоугольника 
-	//ConvexShape convex;
- 
-	// Устанавливаем ему 5 вершин
-	//convex.setPointCount(4);
- 
-	// Устанавливаем координаты вершин
-    //convex.setPoint(0, Vector2f(7.f, 0.f));
-	//convex.setPoint(0, Vector2f(3.f, 0.f));
-	//convex.setPoint(1, Vector2f(0.f, 120.f));
-	//convex.setPoint(2, Vector2f(12.f, 120.f));
-
-    //convex.setOrigin(6,120);
-	
- 
-	// Устанавливаем цвет многоугольника - черный
-	//convex.setFillColor(Color(0,169,200,255));
-    //convex.setOutlineThickness(4.f);
-    //convex.setOutlineColor(Color(10,180,230,155));
-	// Теперь сдвинем его вниз и чуть-чуть вправо
-	//convex.move(450, 250);
-    //convex.rotate(coord_x);
-	// Отрисовка многоугольника
-	//window.draw(convex);
-    
-   
- 
-		// Отрисовка окна
-		window.display();
-        //usleep(20000);
-	}
- 
-	return 0;
+    return 0;
 }
